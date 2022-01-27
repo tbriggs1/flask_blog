c3147faae7661222d6adad6835fd3f18cd58752a
tbriggs1 <tombriggs1993@outlook.com>
2022-01-27 16:07:47 +0000

    Added blog functionality

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -72,8 +72,10 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id)
+            'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
+
+
 
 @bp.route('/logout')
 def logout():
@@ -84,7 +86,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.log'))
+            return redirect(url_for('auth.login'))
 
         return view(**kwargs)
     return wrapped_view
